e22d0ed3894819dd3758610ca9f7ae60262ab0fb
juhyang <ljh28891004@gmail.com>
2021-01-25 19:58:55 +0900

    2021년 1월 25일 월요일 19시 58분 55초 KST

--- New Unity Project/Assets/LifeCycle.cs
+++ New Unity Project/Assets/LifeCycle.cs
@@ -2,41 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LifeCycle : MonoBehaviour
-{
-    // 초기화
-    void Awake() { // 맨 처음 초기화 
-        Debug.Log("플레이어 데이터가 준비되었습니다.");
-    }
-
-    void OnEnable() { // 게임 오브젝트가 활성화 되었을 때
-        Debug.Log("플레이어가 로그인했습니다.");
-    }
-
-    void start() { // 업데이트 시작 직전, 최초 실행 
-        Debug.Log("사냥 장비를 챙겼습니다.");
-    }
-
-    // 물리연산
-    void FixedUpdate () {// 물리 연산 업데이트 => 1초에 여러번 ! 고정된 실행 주기로 CPU를 많이 사용
-        Debug.Log("이동 ~");
-    }
-
+public class LifeCycle : MonoBehaviour {
     // 게임 로직
     void Update () { // 게임 로직 업데이트 => 환경에 따라 실행 주기가 달라질 수 있음.
-        Debug.Log("몬스터 사냥 !!");
-    }
+        if (Input.anyKeyDown) {
+            Debug.Log("플레이어가 아무 키를 눌렀습니다.");
+        }
 
-    void LateUpdate() { // 모든 업데이트 끝난 후
-        Debug.Log("경험치 획득.");
-    }
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            Debug.Log("아이템을 구입하였습니다.");
+        }
 
-    void OnDisable() { // 게임 오브젝트가 비활성화 되었을 때
-        Debug.Log("플레이어가 로그아웃 했습니다.");
-    }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            Debug.Log("왼쪽으로 이동 중");
+        }
 
-    // 해체 영역
-    void OnDestroy() { // 게임 오브젝트가 삭제될 때
-        Debug.Log("플레이어 데이터를 해제하였습니다.");
+        if (Input.GetKeyUp(KeyCode.RightArrow)) {
+            Debug.Log("오른쪽 이동을 멈추었습니다.");
+        }
     }
 }
