f43dc16d1938a9b389589f2d08605daad6a7f43f
juhyang <ljh28891004@gmail.com>
2021-01-25 21:02:06 +0900

    오브젝트 이동

--- New Unity Project/Assets/LifeCycle.cs
+++ New Unity Project/Assets/LifeCycle.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LifeCycle : MonoBehaviour {
-    // 게임 로직
-    void Update () { // 게임 로직 업데이트 => 환경에 따라 실행 주기가 달라질 수 있음.
-        if (Input.anyKeyDown) {
-            Debug.Log("플레이어가 아무 키를 눌렀습니다.");
-        }
+    
+    private void Start() {
+        // 기본 초기화 되어 있는 변수 :: transform        
+        Vector3 vec = new Vector3(5, 0, 0);
+        transform.Translate(vec);
+    }
 
-        if (Input.GetButton("Horizontal")) { // Horizontal -> 좌우 방향키, a, d
-            // GetAxis -> 가중치 있음.
-            // GetAxisRaw -> -1 왼쪽, 1 오른쪽, 0 가운데
-            Debug.Log("횡 이동 중..." + Input.GetAxisRaw("Horizontal"));
-        }
-
-        if (Input.GetButton("Vertical")) { // Vertical -> 상하 방향키, w, s
-            Debug.Log("종 이동 중..." + Input.GetAxisRaw("Vertical"));
-        }
+    private void Update() {
+        Vector3 vec = new Vector3(0, 0.01f, 0);
+        transform.Translate(vec);
     }
 }
