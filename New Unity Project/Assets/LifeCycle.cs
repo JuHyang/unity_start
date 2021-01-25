affcf6adc1c71939c8b5e3155890c9e7f7788c01
juhyang <ljh28891004@gmail.com>
2021-01-25 20:28:35 +0900

    Keyboard horizontal, vertical

--- New Unity Project/Assets/LifeCycle.cs
+++ New Unity Project/Assets/LifeCycle.cs
@@ -9,16 +9,14 @@ public class LifeCycle : MonoBehaviour {
             Debug.Log("플레이어가 아무 키를 눌렀습니다.");
         }
 
-        if (Input.GetButtonDown("Jump")) { 
-            Debug.Log("점프!");
+        if (Input.GetButton("Horizontal")) { // Horizontal -> 좌우 방향키, a, d
+            // GetAxis -> 가중치 있음.
+            // GetAxisRaw -> -1 왼쪽, 1 오른쪽, 0 가운데
+            Debug.Log("횡 이동 중..." + Input.GetAxisRaw("Horizontal"));
         }
 
-        if (Input.GetButton("Jump")) {
-            Debug.Log("점프 모으는 중 ...");
-        }
-
-        if (Input.GetButtonUp("Jump")) {
-            Debug.Log("슈퍼 점프 !!");
+        if (Input.GetButton("Vertical")) { // Vertical -> 상하 방향키, w, s
+            Debug.Log("종 이동 중..." + Input.GetAxisRaw("Vertical"));
         }
     }
 }
