9d00b34f45604678d5ed420d16d40a98c07eea66
juhyang <ljh28891004@gmail.com>
2021-02-18 23:59:07 +0900

    B9 - 이동거리를 공평하게! 델타타임

---
+++ New Unity Project/Assets/DeltaTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeltaTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        Vector3 vec = new Vector3(Input.GetAxisRaw("Horizontal")  * Time.deltaTime, Input.GetAxisRaw("vertical") * Time.deltaTime);
+        transform.Translate(vec);
+    }
+}
