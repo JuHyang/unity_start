86103c936a8a3378e9d72c106f865497791024aa
juhyang <ljh28891004@gmail.com>
2021-02-18 23:54:13 +0900

    코드 정리

---
+++ New Unity Project/Assets/KeyInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInput : MonoBehaviour
+{
+    // 게임 로직
+    void Update () { // 게임 로직 업데이트 => 환경에 따라 실행 주기가 달라질 수 있음.
+        if (Input.anyKeyDown) {
+            Debug.Log("플레이어가 아무 키를 눌렀습니다.");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            Debug.Log("아이템을 구입하였습니다.");
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            Debug.Log("왼쪽으로 이동 중");
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow)) {
+            Debug.Log("오른쪽 이동을 멈추었습니다.");
+        }
+    }
+}
--- New Unity Project/Assets/LifeCycle.cs
+++ New Unity Project/Assets/LifeCycle.cs
@@ -3,22 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LifeCycle : MonoBehaviour {
-    Vector3 target = new Vector3(8, 1.5f, 0);
+    // 초기화
+    void Awake() { // 맨 처음 초기화 
+        Debug.Log("플레이어 데이터가 준비되었습니다.");
+    }
 
-    private void Update() {
-        // 1. MoveTowards
-        // transform.position = Vector3.MoveTowards(transform.position, target, 0.1f);
+    void OnEnable() { // 게임 오브젝트가 활성화 되었을 때
+        Debug.Log("플레이어가 로그인했습니다.");
+    }
 
-        // 2. SmoothDamp
-        // Vector3 velo = Vector3.zero;
+    void start() { // 업데이트 시작 직전, 최초 실행 
+        Debug.Log("사냥 장비를 챙겼습니다.");
+    }
 
-        // transform.position = Vector3.SmoothDamp(transform.position, target, ref velo, 0.2f);
+    // 물리연산
+    void FixedUpdate () {// 물리 연산 업데이트 => 1초에 여러번 ! 고정된 실행 주기로 CPU를 많이 사용
+        Debug.Log("이동 ~");
+    }
 
-        // 3. Lerp (선형 보간)
-        // transform.position = Vector3.Lerp(transform.position, target, 0.1f);
+    // 게임 로직
+    void Update () { // 게임 로직 업데이트 => 환경에 따라 실행 주기가 달라질 수 있음.
+        Debug.Log("몬스터 사냥 !!");
+    }
 
-        // 4. SLerp (구면 선형 보간)
-        transform.position = Vector3.Slerp(transform.position, target, 0.05f);
+    void LateUpdate() { // 모든 업데이트 끝난 후
+        Debug.Log("경험치 획득.");
+    }
 
+    void OnDisable() { // 게임 오브젝트가 비활성화 되었을 때
+        Debug.Log("플레이어가 로그아웃 했습니다.");
+    }
+
+    // 해체 영역
+    void OnDestroy() { // 게임 오브젝트가 삭제될 때
+        Debug.Log("플레이어 데이터를 해제하였습니다.");
     }
 }
