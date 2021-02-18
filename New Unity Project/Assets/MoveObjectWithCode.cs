1e4d2b512566bdb5718a86d7488a4377978407dd
juhyang <ljh28891004@gmail.com>
2021-02-18 23:55:34 +0900

    코드 정리

---
+++ New Unity Project/Assets/MoveObjectWithCode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjectWithCode : MonoBehaviour
+{
+    ector3 target = new Vector3(8, 1.5f, 0);
+
+    private void Update() {
+        // 1. MoveTowards
+        // transform.position = Vector3.MoveTowards(transform.position, target, 0.1f);
+
+        // 2. SmoothDamp
+        // Vector3 velo = Vector3.zero;
+
+        // transform.position = Vector3.SmoothDamp(transform.position, target, ref velo, 0.2f);
+
+        // 3. Lerp (선형 보간)
+        // transform.position = Vector3.Lerp(transform.position, target, 0.1f);
+
+        // 4. SLerp (구면 선형 보간)
+        transform.position = Vector3.Slerp(transform.position, target, 0.05f);
+
+    }
+}
