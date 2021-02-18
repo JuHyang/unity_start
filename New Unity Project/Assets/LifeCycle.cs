2cfc342bac04defdb525c7d562c1dc3b4f8e8bbf
juhyang <ljh28891004@gmail.com>
2021-02-18 23:46:50 +0900

    B7 목표 지점으로 이동시키기

--- New Unity Project/Assets/LifeCycle.cs
+++ New Unity Project/Assets/LifeCycle.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LifeCycle : MonoBehaviour {
-    
-    private void Start() {
-        // 기본 초기화 되어 있는 변수 :: transform        
-        Vector3 vec = new Vector3(5, 0, 0);
-        transform.Translate(vec);
-    }
+    Vector3 target = new Vector3(8, 1.5f, 0);
 
     private void Update() {
-        Vector3 vec = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        transform.Translate(vec);
+        // 1. MoveTowards
+        // transform.position = Vector3.MoveTowards(transform.position, target, 0.1f);
+
+        // 2. SmoothDamp
+        // Vector3 velo = Vector3.zero;
+
+        // transform.position = Vector3.SmoothDamp(transform.position, target, ref velo, 0.2f);
+
+        // 3. Lerp (선형 보간)
+        // transform.position = Vector3.Lerp(transform.position, target, 0.1f);
+
+        // 4. SLerp (구면 선형 보간)
+        transform.position = Vector3.Slerp(transform.position, target, 0.05f);
+
     }
 }
